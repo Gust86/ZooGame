0ca938a39771eead063df30de8d84f1c9bacda0d
Gust <ody.zafeirakis@gmail.com>
2020-01-15 23:23:35 +0200

    chore(animal-list): add distinct animals to list

--- zoogame/src/app/animal-list.ts
+++ zoogame/src/app/animal-list.ts
@@ -1,27 +1,31 @@
 import { Animal } from './models/animal';
 
+export const elephant: Animal = {
+    name: 'elephant',
+    sound: '',
+    image: './assets/elephant.jpeg',
+    stamina: 150,
+    staminaDecay: 2000
+}
+export const tiger: Animal = {
+    name: 'tiger',
+    sound: '',
+    image: './assets/tiger.jfif',
+    stamina: 100,
+    staminaDecay: 2000   
+}
+export const monkey: Animal = {
+    name: 'monkey',
+    sound: '',
+    image: './assets/monkey.jfif',
+    stamina: 60,
+    staminaDecay: 2000
+}
 // Δηλώνουμε μια const variable τύπου Animal[] (δηλαδή μια λίστα με objects τύπου Animal)
 // Δίνουμε τιμές στο κάθε object μέσα στη λίστα για τις ανάγκες του παιχνιδιού ίσα ίσα για να έχουμε κάτι να χρησιμοποιήσουμε/εμφανίσουμε
 export const animals: Animal[] = [
-    {
-        name: 'tiger',
-        sound: '',
-        image: './assets/tiger.jfif',
-        stamina: 100,
-        staminaDecay: 2000   
-    },
-    {
-        name: 'monkey',
-        sound: '',
-        image: './assets/monkey.jfif',
-        stamina: 60,
-        staminaDecay: 2000
-    },
-    {
-        name: 'elephant',
-        sound: '',
-        image: './assets/elephant.jpeg',
-        stamina: 150,
-        staminaDecay: 2000
-    }
+    elephant, tiger, monkey
 ]
+
+
+
